63bc66423947e45e8e540be65dbe0676db2b4dd3
denchec <dencheses@gmail.com>
2024-07-12 20:08:11 +0300

    optional schemas

--- schemas.py
+++ schemas.py
@@ -2,49 +2,49 @@ from pydantic import BaseModel
 
 
 class Base(BaseModel):  # Unit
-    attack: int
-    health: int
-    id: str
-    isHead: bool
-    lastAttack: str = None
-    range: int
-    x: int
-    y: int
+    attack: Optional[int]
+    health: Optional[int]
+    id: Optional[str]
+    isHead: Optional[bool]
+    lastAttack: Optional[str] = None
+    range: Optional[int]
+    x: Optional[int]
+    y: Optional[int]
 
 
 class Player(BaseModel):  # Unit
-    enemyBlockKills: int
-    gameEndedAt: str = None
-    gold: int = None
-    name: str
-    points: int
-    zombieKills: int
+    enemyBlockKills: Optional[int]
+    gameEndedAt: Optional[str] = None
+    gold: Optional[int] = None
+    name: Optional[str]
+    points: Optional[int]
+    zombieKills: Optional[int]
 
 
 class Zombies(BaseModel):  # Unit
-    attack: int
-    direction: str
-    health: int
-    id: str
-    speed: int
-    type: str
-    waitTurns: int
-    x: int
-    y: int
+    attack: Optional[int]
+    direction: Optional[str]
+    health: Optional[int]
+    id: Optional[str]
+    speed: Optional[int]
+    type: Optional[str]
+    waitTurns: Optional[int]
+    x: Optional[int]
+    y: Optional[int]
 
 
 class Zpots(BaseModel):  # World
-    type: str
-    x: str
-    y: str
+    type: Optional[str]
+    x: Optional[str]
+    y: Optional[str]
 
 
 class Units(BaseModel):
-    base: Base
-    enemyBlocks: None
-    player: Player
-    zombies: list[Zombies]
-    zpots: list[Zpots]
-    realmName: str
-    turn: int
-    turnEndsInMs: int
+    base: Optional[Base]
+    enemyBlocks: Optional[None]
+    player: Optional[Player]
+    zombies: Optional[list[Zombies]]
+    zpots: Optional[list[Zpots]]
+    realmName: Optional[str]
+    turn: Optional[int]
+    turnEndsInMs: Optional[int]
