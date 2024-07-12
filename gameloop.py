f37ddc7b940083585d32c32de9a19867e565e0da
BrittleFoot <buftioo@gmail.com>
2024-07-12 21:07:49 +0400

    ds

--- gameloop.py
+++ gameloop.py
@@ -15,7 +15,7 @@ class World:
 
     def update(self):
         self.raw_data = self.client.units()
-        self.units = Units.model_validate()
+        self.units = Units.model_validate(self.raw_data)
         return self.raw_data["turn"], self.raw_data["turnEndsInMs"]
 
 
