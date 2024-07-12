441599707a0f664a1635cb4815711820c0b9054a
21361942 <NAleksPermyakova@sberbank.ru>
2024-07-12 19:50:02 +0300

    wip

--- test_nata.py
+++ test_nata.py
@@ -17,17 +17,54 @@ command = {
 units = api_test.units()
 pprint(units)
 
-for item in units['base']:
-    command['attack'].append(
-        {
-            "blockId": item['id'],
-            "target": {
-                "x": item['zombies'][0]['x'],
-                "y": item['zombies'][0]['x'],
-            }}
-    )
+base_x = []
+base_y =[]
+
+for base_block in units['base']:
+    for zombie in units['zombies']:
+        if base_block['x'] +5 > zombie['x'] > base_block['x'] -5 \
+                and base_block['y'] +5 > zombie['y'] > base_block['y'] -5:
+            command['attack'].append(
+            {
+                "blockId": base_block['id'],
+                "target": {
+                    "x": zombie['x'],
+                    "y": zombie['y'],
+                }}
+        )
+
+    base_x.append(base_block['x'])
+    base_y.append(base_block['y'])
 
 
+
+for i in range(units['player']['gold']):
+    for x in base_x:
+        for y in base_y:
+            command['build'].append(
+                {
+                    "x": x-1,
+                    "y": y,
+                }
+            )
+            command['build'].append(
+                {
+                    "x": x,
+                    "y": y - 1,
+                }
+            )
+            command['build'].append(
+                {
+                    "x": x + 1,
+                    "y": y,
+                }
+            )
+            command['build'].append(
+                {
+                    "x": x,
+                    "y": y + 1,
+                }
+            )
 # ≈
 
 pprint(api_test.command(command))
