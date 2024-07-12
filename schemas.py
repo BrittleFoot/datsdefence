1c35519687b31bb79b93bf6d303a017c4b9b1236
BrittleFoot <buftioo@gmail.com>
2024-07-12 21:00:36 +0400

    + sch

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from schemas import BaseModel
 
 
 class Base(BaseModel):  # Unit
@@ -37,24 +37,6 @@ class Zpots(BaseModel):  # World
     type: str
     x: str
     y: str
-
-
-{
-    "base": None,
-    "enemyBlocks": None,
-    "player": {
-        "enemyBlockKills": 0,
-        "gameEndedAt": "2024-07-12T16:45:56.464926635Z",
-        "gold": 10,
-        "name": "ГомЛомНом",
-        "points": 0,
-        "zombieKills": 0,
-    },
-    "realmName": "test-day1-3",
-    "turn": 184,
-    "turnEndsInMs": 1425,
-    "zombies": None,
-}
 
 
 class Units(BaseModel):
