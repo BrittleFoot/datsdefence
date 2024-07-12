67649e2c7f92513c7487fc72e60b3779b7237a17
denchec <dencheses@gmail.com>
2024-07-12 19:36:36 +0300

    unit pydantic

---
+++ pydantic.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+
+
+class Base(BaseModel):  # Unit
+    attack: int
+    health: int
+    id: str
+    isHead: bool
+    lastAttack: str = None
+    range: int
+    x: int
+    y: int
+
+
+class Player(BaseModel):  # Unit
+    enemyBlockKills: int
+    gameEndedAt: str = None
+    gold: int = None
+    name: str
+    points: int
+    zombieKills: int
+
+
+class Zombies(BaseModel):  # Unit
+    attack: int
+    direction: str
+    health: int
+    id: str
+    speed: int
+    type: str
+    waitTurns: int
+    x: int
+    y: int
---
+++ test_denchec.py
@@ -0,0 +1,6 @@
+from pprint import pprint
+
+from client import api_test
+
+# pprint(api_test.world())
+pprint(api_test.units())
