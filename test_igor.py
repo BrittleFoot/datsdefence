25a8f69374685350ed5c23ebc48789673dd756f3
BrittleFoot <buftioo@gmail.com>
2024-07-12 20:50:31 +0400

    + gameloop

--- test_igor.py
+++ test_igor.py
@@ -1,14 +1,9 @@
-from pprint import pprint
-
-from client import api_test
 from gameloop import GameLoop
-
-pprint(api_test.rounds())
 
 
 class MyLoop(GameLoop):
     def loop_body(self):
-        print("something to do")
+        print(self.turn)
 
 
 MyLoop(is_test=True).just_run_already()
